2eff706b09192f4c18b9324722542c840faf9dbf
Gdagdelen <gokaydagdelen@gmail.com>
2020-12-19 14:54:04 -0300

    Adding Home and StateProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router ,Switch , Route } from "react-router-dom";
 import Header from "./header/Header";
-
+import Home from "./home/Home";
 
 // Switch is used for identigying unique part
 // without switch (/ because of this), we will see Home in every page
@@ -14,7 +14,8 @@ function App() {
    <Switch>
       <Route path="/login">Login</Route>
       <Route path="/register">Register</Route>
-      <Route path="/"></Route>
+      <Route path="/" component={Home} /> 
+      {/* <Route path="/">Home</Route> */}
      </Switch>
    </div>
  </Router>
